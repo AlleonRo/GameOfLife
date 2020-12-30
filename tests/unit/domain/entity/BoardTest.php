1e9b1f95b04dbc8e140ca6867fdab26fbcae4039
alenrodriguez <alenrodriguez@vente-privee.com>
2020-12-30 10:28:23 +0100

    Fix dynamic get class

--- tests/unit/domain/entity/BoardTest.php
+++ tests/unit/domain/entity/BoardTest.php
@@ -7,6 +7,8 @@ use GameofLife\domain\entity\Cell;
 use GameofLife\domain\exception\CoordinateNotValid;
 use PHPUnit\Framework\TestCase;
 
+use function get_class;
+
 class BoardTest extends TestCase
 {
     /**
@@ -16,7 +18,7 @@ class BoardTest extends TestCase
     public function createBoard(): void
     {
         $board = Board::createRandom(1, 1);
-        self::assertEquals(Cell::class, $board->getCell(0, 0)::class);
+        self::assertEquals(Cell::class, get_class($board->getCell(0, 0)));
         self::assertEquals(1, $board->getHeight());
         self::assertEquals(1, $board->getWidth());
     }
