b35707085ef4b11b1dd6e8f7a3fb2add0a090a32
alenrodriguez <alenrodriguez@vente-privee.com>
2020-12-30 10:27:43 +0100

    Fix visibility constant

--- src/domain/enum/NeighboursRanges.php
+++ src/domain/enum/NeighboursRanges.php
@@ -4,5 +4,5 @@ namespace GameofLife\domain\enum;
 
 final class NeighboursRanges
 {
-    const RANGES = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
+    public const RANGES = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
 }
